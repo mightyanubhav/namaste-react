dbbe90204e97699282f082334aa7f34b1a395eec
Anubhav Shukla <kumaranubhav691@gmail.com>
2024-09-12 15:47:01 +0530

    new change

--- App.js
+++ App.js
@@ -1,31 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (
-    <h1 id="heading"> 
-        Nameste React using jsx
-    </h1>
-) ;
-console.log(jsxHeading);
+// const jsxHeading = (
+//     <h1 id="heading"> 
+//         Nameste React using jsx
+//     </h1>
+// ) ;
+// console.log(jsxHeading);
 
-const JsxHeading = () => <h1>Hello brother</h1>
+// const JsxHeading = () => <h1>Hello brother</h1>
 
 
 
-// React Component 
+// // React Component 
 
-const HeadingComponent = () => {
-return(
-        <div className="first">
-            <h1 className="heading"> Nameste React functional Component</h1>
-            <h1 className="heading"> Nameste React functional Component</h1>
-            <h1 className="heading"> Nameste React functional Component</h1>     
-        </div>
+// const HeadingComponent = () => {
+// return(
+//         <div className="first">
+//             <h1 className="heading"> Nameste React functional Component</h1>
+//             <h1 className="heading"> Nameste React functional Component</h1>
+//             <h1 className="heading"> Nameste React functional Component</h1>     
+//         </div>
         
+//     )
+// };
+// const HeadingComponent2 = () => <h1>Again react functional Component</h1>;
+// const HeadingComponent3 = () => true;
+
+
+
+const change = <h1 className="heading"> this is the jsx file</h1>;
+const bigChange = (
+    <div className = "name">
+        <div className = "name"></div>
+    </div>
+);
+
+const SmallComponent = () => <p>Henry Griffin loves louis griffin</p>;
+const ReactComponent = () => {
+    return (
+        <div>
+            <SmallComponent/>
+            <h1> This is my file </h1>
+            <h1> This is my file </h1>
+            <h1> This is my file </h1>
+        </div>
+
     )
-};
-const HeadingComponent2 = () => <h1>Again react functional Component</h1>;
-const HeadingComponent3 = () => true;
+}
+;
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<JsxHeading></JsxHeading>);
+root.render(<ReactComponent/>);
+
