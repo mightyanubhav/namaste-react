036b00ef8054332650494b17dacceee04bd932d9
Anubhav Shukla <kumaranubhav691@gmail.com>
2024-09-12 13:27:37 +0530

    nextCommit

--- App.js
+++ App.js
@@ -1,17 +1,2 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const head_1 = React.createElement('h1', {}, 'this is first h1');
-const head_2 = React.createElement('h1', {}, "this is second h1");
-const head_3 = React.createElement('h3', {}, 'this is h2 named h3');
-
-
-const child1 = React.createElement('div', {id:'child1'}, [head_1, head_2, head_3]);
-const child2 = React.createElement('div', {id:'child2'}, [head_1, head_2, head_3]);
-
-const parent = React.createElement('div', {id:'parent'}, [child1,child2]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
-console.log(parent);
