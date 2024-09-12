3f97b49da92f226d302c869d1ebb9ea723700d8e
Anubhav Shukla <kumaranubhav691@gmail.com>
2024-09-12 15:20:27 +0530

    changes

--- App.js
+++ App.js
@@ -1,2 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const JsxHeading = () =>(
+    <h1 id="heading"> 
+        Nameste React using jsx
+    </h1>
+) ;
+console.log(jsxHeading);
+
+
+
+// React Component 
+
+const HeadingComponent = () => {
+return(
+        <div className="first">
+            <h1 className="heading"> Nameste React functional Component</h1>
+            <h1 className="heading"> Nameste React functional Component</h1>
+            <h1 className="heading"> Nameste React functional Component</h1>     
+        </div>
+        
+    )
+};
+const HeadingComponent2 = () => <h1>Again react functional Component</h1>;
+const HeadingComponent3 = () => true;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(JsxHeading);
