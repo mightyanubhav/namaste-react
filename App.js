b69d4c22817c0095be2e1bea795185d17c015ded
Anubhav Shukla <kumaranubhav691@gmail.com>
2024-09-13 11:09:29 +0530

    second commit 13 sep

--- App.js
+++ App.js
@@ -1,36 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-
-const LeftLogo = () =>{
-    return <div id="container1">
-        <img src="https://play-lh.googleusercontent.com/pfvYf8MWEUgO4Ip9W5iUkyCMo7JH_Fhu1f7YMK6mb7Olw_xzacJOe1XwtPXDE04GaA" alt="no image"></img>
-    </div>
-}
-const SearchBar = () =>{
-    return <div id="search">
-        <input type="text" placeholder="Search.."></input>
-    </div>
-}
-const UserIcon = ()=>{
-    return <div id="container2">
-        <img src="https://avatars.githubusercontent.com/u/12824231?v=4" alt=""></img>
-    </div>
-}
-const ComponentInsideComponent = () =>{
+const Header = () =>{
     return (
-        <div id="top">
-            <LeftLogo/>
-            <SearchBar/>
-            <UserIcon/>
+        <div className="header">
+            <div className="logo-container">
+                <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/burger-logo-design-template-8f3596992e540bb9078f91f59f745ce2_screen.jpg?ts=1653317642"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
     );
 }
 
-const ReactHeader = () =>{
-    return  <ComponentInsideComponent/>;
- }
+const  RestaurantCard = () =>{
+    return (
+        <div className="res-card">
+            <div className="img-container">
+                <img
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/d6cc715e5180e7b29674f22383118cc6"
+                alt="no image" />
+            </div>
+            <div className="text-container">
+                <h3> Meghana Foods </h3>
+            </div>
+        </div>
+    );
+}
+const Body = () =>{
+    return (
+        <div className="body">
+            <div className="search">
+                Search
+            </div>
+            <div className="res-container">
+                
+                <RestaurantCard/>
+            </div>
+        </div>
+    );
+}
+const AppLayout = () =>{
+    return (
+        <div className="app">
+            <Header />
+            <Body/>
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ReactHeader/>);
+root.render(<AppLayout/>);
